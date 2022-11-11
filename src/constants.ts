d942ab64e9db606c6b364a55558bd4d0d0688fb6
Boris Tsao <boris.tsao@gmail.com>
2022-11-11 07:39:09 -0800

    adding auto-animte form and creating games pages

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,18 @@
 export const CONTRACT_ID = "dev-1668144018475-40094893174123";
+
+export const WEEK_DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export const MONTH_NAMES = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
