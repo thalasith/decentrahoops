e9c65c5dae30f58d7bddbd2a23757c3f7685d913
Boris Tsao <boris.tsao@gmail.com>
2022-11-12 18:56:15 -0800

    finalizing the game header

--- src/pages/games/[gameId].tsx
+++ src/pages/games/[gameId].tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Header from "../../components/Header";
@@ -6,15 +5,21 @@ import { WalletSelectorContextProvider } from "../../contexts/WalletSelectorCont
 import { trpc } from "../../utils/trpc";
 import { useRouter } from "next/router";
 import { RiArrowLeftSFill, RiArrowRightSFill } from "react-icons/ri";
+import { useWindowWidth } from "@react-hook/window-size";
+import Link from "next/link";
+import { RiArrowGoBackFill } from "react-icons/ri";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { gameId } = router.query as { gameId: string };
+  const windowWidth = useWindowWidth();
 
   const boxscore = trpc.nbaGames.gameById.useQuery({
     gameId: gameId,
   });
 
+  const scoresNotStarted = ["0", "0", "0", "0", "0"];
+  console.log(boxscore);
   // TODO: Mobile optimize
   return (
     <>
@@ -26,110 +31,152 @@ const Home: NextPage = () => {
       <main className="bg-gray-800 text-white">
         <WalletSelectorContextProvider>
           <Header />
-          {boxscore.isSuccess && (
-            <div className="container mx-auto flex min-h-screen flex-col items-center p-4">
-              <div className=" flex w-10/12 flex-row items-center justify-center rounded bg-orange-200 text-slate-700">
-                <div className="my-2 mr-4 flex w-1/3 items-center justify-end">
-                  <div className="flex flex-col">
-                    <span className="text-end text-2xl font-semibold">
-                      {boxscore?.data.competitors[1].team.displayName}
+          <div className="container mx-auto flex min-h-screen flex-col">
+            <Link href="/games" className="mx-auto my-4 w-full lg:w-10/12">
+              <button className="mx-1 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-orange-600 px-1 py-1 text-base font-medium text-white shadow-sm hover:bg-orange-400">
+                <RiArrowGoBackFill className="my-1" />
+                Back to all Games
+              </button>
+            </Link>
+            {boxscore.isSuccess && (
+              <div className="mx-auto flex w-full flex-col items-center justify-center rounded bg-orange-200 text-slate-700 lg:w-10/12">
+                <span> {boxscore?.data.status.type.detail}</span>
+                <div className="flex w-full flex-row">
+                  <div className=" flex w-1/2 items-center justify-end lg:mr-4 lg:w-2/5">
+                    <div className="flex flex-col">
+                      <span className="text-end font-semibold lg:text-2xl">
+                        {windowWidth > 500
+                          ? boxscore?.data.competitors[1].team.displayName
+                          : boxscore?.data.competitors[1].team.abbreviation}
+                      </span>
+                      <span className="text-end text-xs">
+                        {boxscore?.data.competitors[1].record[0].displayValue}
+                        {windowWidth > 500 &&
+                          ", " +
+                            boxscore?.data.competitors[1].record[1]
+                              .displayValue +
+                            " AWAY"}
+                      </span>
+                    </div>
+                    <img
+                      className="h-12 w-12 lg:h-24 lg:w-24"
+                      src={boxscore?.data.competitors[1].team.logos[0].href}
+                    />
+                    <span
+                      className={`ml-2 flex items-center font-bold lg:text-4xl ${
+                        boxscore?.data.competitors[1].winner
+                          ? "text-black"
+                          : "text-gray-500"
+                      }`}
+                    >
+                      {boxscore?.data.competitors[1].score}
+                      {boxscore?.data.competitors[1].winner && (
+                        <RiArrowLeftSFill />
+                      )}
                     </span>
-                    <span className="text-end text-xs">2-8, 2-2 AWAY</span>
                   </div>
-                  <img
-                    style={{ width: "110px", height: "110px" }}
-                    src={boxscore?.data.competitors[1].team.logos[0].href}
-                  />
-                  <span
-                    className={`ml-2 flex text-4xl font-bold ${
-                      boxscore?.data.competitors[1].winner
-                        ? "text-black"
-                        : "text-gray-500"
-                    }`}
-                  >
-                    {boxscore?.data.competitors[1].score}
-                    {boxscore?.data.competitors[1].winner && (
-                      <RiArrowLeftSFill />
-                    )}
-                  </span>
-                </div>
-                <div className="mx-2 flex w-1/3 flex-col items-center justify-center">
-                  <span>FINAL</span>
-                  <table className="table-auto">
-                    <thead>
-                      <tr className="border-b border-slate-400">
-                        <th className="pr-2"></th>
-                        <th className="px-2">1</th>
-                        <th className="px-2">2</th>
-                        <th className="px-2">3</th>
-                        <th className="px-2">4</th>
-                        {boxscore?.data.status.type.detail === "Final/OT" && (
-                          <th className="px-2">OT</th>
-                        )}
-                        <th className="px-2 font-semibold">T</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td className="pr-2">
-                          {boxscore?.data.competitors[1].team.abbreviation}
-                        </td>
-                        {boxscore?.data.competitors[1].linescores.map(
-                          (line: { displayValue: string }, i: number) => (
-                            <td key={i} className="px-2">
-                              {line.displayValue}
-                            </td>
-                          )
-                        )}
-                        <td className="px-2 font-semibold">
-                          {boxscore?.data.competitors[1].score}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="pr-2">
-                          {boxscore?.data.competitors[0].team.abbreviation}
-                        </td>
-                        {boxscore?.data.competitors[0].linescores.map(
-                          (line: { displayValue: string }, i: number) => (
-                            <td key={i} className="px-2">
-                              {line.displayValue}
-                            </td>
-                          )
-                        )}
-                        <td className="px-2 font-semibold">
-                          {boxscore?.data.competitors[0].score}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <div className="ml-4 flex w-1/3 flex-row items-center justify-start">
-                  <span
-                    className={`ml-2 flex text-4xl font-bold ${
-                      boxscore?.data.competitors[0].winner
-                        ? "text-black"
-                        : "text-gray-500"
-                    }`}
-                  >
-                    {boxscore?.data.competitors[0].winner && (
-                      <RiArrowRightSFill />
-                    )}
-                    {boxscore?.data.competitors[0].score}
-                  </span>
-                  <img
-                    style={{ width: "110px", height: "110px" }}
-                    src={boxscore?.data.competitors[0].team.logos[0].href}
-                  />
-                  <div className="flex flex-col">
-                    <span className="text-start text-2xl font-semibold">
-                      {boxscore?.data.competitors[0].team.displayName}
+                  <div className="mx-2 hidden w-1/5 flex-col items-center justify-center lg:flex">
+                    <table className="table-auto">
+                      <thead>
+                        <tr className="border-b border-slate-400">
+                          <th className="pr-2"></th>
+                          <th className="px-2">1</th>
+                          <th className="px-2">2</th>
+                          <th className="px-2">3</th>
+                          <th className="px-2">4</th>
+                          {boxscore?.data.status.type.detail === "Final/OT" && (
+                            <th className="px-2">OT</th>
+                          )}
+                          <th className="px-2 font-semibold">T</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td className="pr-2">
+                            {boxscore?.data.competitors[1].team.abbreviation}
+                          </td>
+                          {boxscore?.data.competitors[1].linescores
+                            ? boxscore?.data.competitors[1].linescores.map(
+                                (line: { displayValue: string }, i: number) => (
+                                  <td key={i} className="px-2">
+                                    {line.displayValue}
+                                  </td>
+                                )
+                              )
+                            : scoresNotStarted.map(
+                                (line: string, i: number) => (
+                                  <td key={i} className="px-2">
+                                    {line}
+                                  </td>
+                                )
+                              )}
+                          <td className="px-2 font-semibold">
+                            {boxscore?.data.competitors[1].score}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td className="pr-2">
+                            {boxscore?.data.competitors[0].team.abbreviation}
+                          </td>
+                          {boxscore?.data.competitors[0].linescores
+                            ? boxscore?.data.competitors[0].linescores.map(
+                                (line: { displayValue: string }, i: number) => (
+                                  <td key={i} className="px-2">
+                                    {line.displayValue}
+                                  </td>
+                                )
+                              )
+                            : scoresNotStarted.map(
+                                (line: string, i: number) => (
+                                  <td key={i} className="px-2">
+                                    {line}
+                                  </td>
+                                )
+                              )}
+                          <td className="px-2 font-semibold">
+                            {boxscore?.data.competitors[0].score}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div className="flex w-1/2 flex-row items-center justify-start lg:ml-4 lg:w-2/5">
+                    <span
+                      className={`ml-2 flex items-center font-bold lg:text-4xl ${
+                        boxscore?.data.competitors[0].winner
+                          ? "text-black"
+                          : "text-gray-500"
+                      }`}
+                    >
+                      {boxscore?.data.competitors[0].winner && (
+                        <RiArrowRightSFill />
+                      )}
+                      {boxscore?.data.competitors[0].score}
                     </span>
-                    <span className="text-start text-xs">2-8, 2-2 HOME</span>
+                    <img
+                      className="h-12 w-12 lg:h-24 lg:w-24"
+                      src={boxscore?.data.competitors[0].team.logos[0].href}
+                    />
+                    <div className="flex flex-col">
+                      <span className="text-start font-semibold lg:text-2xl">
+                        {windowWidth > 500
+                          ? boxscore?.data.competitors[0].team.displayName
+                          : boxscore?.data.competitors[0].team.abbreviation}
+                      </span>
+                      <span className="text-start text-xs">
+                        {boxscore?.data.competitors[0].record[0].displayValue}
+                        {windowWidth > 500 &&
+                          ", " +
+                            boxscore?.data.competitors[0].record[1]
+                              .displayValue +
+                            " HOME"}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </WalletSelectorContextProvider>
       </main>
     </>
